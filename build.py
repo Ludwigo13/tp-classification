a7507f67b223cc1344b0830e00d63514b47f2e12
Ludwigo13 <ludoduclos@hotmail.com>
2024-11-02 12:38:13 -0400

    final?
    +metrics
    +feature selection

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 print("Loading Modules")
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
@@ -44,8 +44,14 @@ utils.save_dataframe_head_as_png(x, f'docs/{filename}')
 report['Data']['Pre-Processing']['Scaling'] = filename
 
 print(" Encode")
+label_encoders = {}
 cat_variables = ['PetType', 'Breed', 'Color']
-x = pd.get_dummies(x, columns=cat_variables, prefix=cat_variables)
+#x = pd.get_dummies(x, columns=cat_variables, prefix=cat_variables)
+for col in cat_variables:
+    le = LabelEncoder()
+    x[col] = le.fit_transform(x[col])
+    label_encoders[col] = le
+    report['Data']['Pre-Processing']['Encoding'] = f'{col} : {list(le.classes_)}'
 encoder = OrdinalEncoder(categories=[['Small', 'Medium', 'Large']])
 x['Size'] = encoder.fit_transform(x[['Size']])
 filename = 'df_encode.png'
@@ -59,34 +65,21 @@ filename = 'df_bool.png'
 utils.save_dataframe_head_as_png(x, f'docs/{filename}')
 report['Data']['Pre-Processing']['Set 0/1 to Bool'] = filename
 
-print(" Split data for training/test")
-report['Data']['Train/test split'] = {}
-x_train, x_test, y_train, y_test = train_test_split(x, target, test_size=0.2, random_state=42)
-report['Data']['Train/test split']['Train individuals'] = len(x_train)
-report['Data']['Train/test split']['Test individuals'] = len(x_test)
-
+# Feature selection based on random forest importances
+selected_features = ['Size', 'AgeMonths', 'Vaccinated', 'HealthCondition', 'AdoptionFee', 'WeightKg', 'TimeInShelterDays', 'Breed']
+selected_x = x[selected_features]
 
 print("Models creation")
 report['Models'] = {}
-
+report['Models']['Selected Features'] = list(selected_features)
+print(" Split data for training/test")
+x_train, x_test, y_train, y_test = train_test_split(selected_x, target, test_size=0.2, random_state=42)
+report['Models']['Train individuals'] = len(x_train)
+report['Models']['Test individuals'] = len(x_test)
 print(" RandomForest")
 report['Models']['RandomForest'] = {}
-print("  Find best estimators")
-n_range = range(50, 70)
-scores = []
-for n in n_range:
-    classifier = RandomForestClassifier(n_estimators=n, random_state=13)
-    classifier.fit(x_train, y_train)
-    y_pred = classifier.predict(x_test)
-    scores.append(accuracy_score(y_test, y_pred))
-plt.plot(n_range, scores)
-plt.xlabel('Value of n_estimators for RandomForestClassifier')
-plt.ylabel('Testing Accuracy')
-fig_path = 'plot_BestEstimators.png'
-plt.savefig(f"docs/{fig_path}")
-report['Models']['RandomForest']['Best estimators'] = fig_path
-print("   Build model")
-RFclassifier = RandomForestClassifier(n_estimators=140, random_state=42)
+print("  Build model")
+RFclassifier = RandomForestClassifier(random_state=42)
 RFclassifier.fit(x_train, y_train)
 y_pred = RFclassifier.predict(x_test)
 conf_matrix = confusion_matrix(y_test, y_pred)
@@ -130,7 +123,6 @@ report['Models']['Naive Bayes']['Accuracy'] = '{:.2f} %'.format(accuracy)
 report['Models']['Naive Bayes']['Precision'] = '{:.2f} %'.format(precision)
 report['Models']['Naive Bayes']['Sensibilite'] = '{:.2f} %'.format(recall)
 report['Models']['Naive Bayes']['F1-Score'] = f1
-
 
 with open('docs/report.json', 'w') as file:
     file.write(json.dumps(report))
